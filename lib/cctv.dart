1f3ba16aec23967c912c008195f119ac2652cac6
arkanfaiz <faizmuaafi@gmail.com>
2025-02-12 11:37:10 +0700

    firman

--- lib/cctv.dart
+++ lib/cctv.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_windows/webview_windows.dart';
 
-class Cctv extends StatelessWidget {
+class Cctv extends StatefulWidget {
   const Cctv({super.key});
+
+  @override
+  State<Cctv> createState() => _CctvState();
+}
+
+class _CctvState extends State<Cctv> {
+  final _controller = WebviewController();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeWebView();
+  }
+
+  Future<void> _initializeWebView() async {
+    await _controller.initialize();
+    await _controller.loadUrl('https://media.lewatmana.com/cam/lintek/192/videoclip20250212_112855.384.mp4');
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard CCTV'),
+        title: Text('Dashboard CCTV'),
       ),
-      body: const WebView(
-        initialUrl: 'https://lewatmana.com/cam/',
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      body: _controller.value.isInitialized
+          ? Webview(_controller)
+          : Center(child: CircularProgressIndicator()),
     );
   }
 }
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Cctv(),
   ));
 }
